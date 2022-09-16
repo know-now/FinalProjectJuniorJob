fa6431422672f1bcc2cb7330996b61b40cbda876
Kusuma <kusumaraju777@gmail.com>
2022-09-16 14:03:49 +0200

    Template blade update

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,13 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="This is our beautiful site">
-    <meta name="author" content="Kusuma, Iveta, Margo, Bricia, Denis">
-    <link rel="stylesheet" href="/resources/css/nav.css">
-        <title>Find your Junior</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka&family=League+Spartan&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/footer_style.css">
+    <link rel="stylesheet" href="/css/navbar_style.css">
+    <title>@yield('title')</title>
+    @yield('css')
 </head>
 
 <body>
+    <nav>
+        <div class="navbar-container">
+            <div class="navbar-image">
+                <a class="navbar-brand" href="">
+                    <img src="{{ URL::asset('/assets/find_your_junior_black sm.png') }}" alt="Bootstrap">
+                </a>
+            </div>
+            <div class="navbar-ul">
+                <ul>
+                    <li>
+                        <a href="" class="button">Register</a>
+                    </li>
+                    <li>
+                        <a href="" class="button">LogIn</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <nav>
         <li><a href="/adem">Work permit in ADEM</a></li>
@@ -24,7 +49,55 @@
         <li><a href="/grade">Test Results</a></li>
         <li><a href="/junior_details">Info about Junior</a></li>
     </nav>
-@yield('content')
+    
+    <div class="content">
+        @yield('content')
+    </div>
+
+
+
+<footer>
+    <div class="container text-center">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-sm-4 col-md-4">
+                <div class="footer-logo">
+                    <img src="{{ URL::asset('/assets/find_your_junior_black.png') }}" alt="">
+                </div>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="footer-heading">
+                    <h3>About Us</h3>
+                </div>
+                <p>A Place to Find The Talent of Tommorrow </p>
+                <p>Platform for Juniors to start carrer</p>
+                <p></p>
+                <p></p>
+            </div>
+            <div class="col-sm-4 col-md-4">
+                <div class="footer-heading">
+                    <h3>Contact</h3>
+                </div>
+                <p>info@findyourjunior.io</p>
+                <p>+352 123 456 789</p>
+
+                <ul>
+                    <li><a href=""><span class="fab fa-facebook" aria-hidden="true"></span> <span
+                                class="sr-only">Facebook</span></a></li>
+                    <li><a href=""><span class="fab fa-twitter" aria-hidden="true"></span> <span
+                                class="sr-only">Tweeter</span></a></li>
+                    <li><a href=""><span class="fab fa-linkedin-in" aria-hidden="true"></span> <span
+                                class="sr-only">LinkedIn</span></a></li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+</script>
 </body>
 
 </html>
