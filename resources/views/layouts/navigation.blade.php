11da00772cbde60dd9cff180d455cc1c324ef910
Margo <106470282+grasser26@users.noreply.github.com>
2022-09-23 16:07:47 +0200

    Delete link of messenger

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -45,9 +45,7 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="/dashboard">Temp Dashboard</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/messages">Messenger</a>
-                                </li>
+                              
 
                             </ul>
                         </div>
